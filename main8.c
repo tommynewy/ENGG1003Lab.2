aff52d8264c4b4b6573fa467d1b7d13de85946e2
Tom Vidler <ctgaminga@gmail.com>
2019-04-30 01:38:28 +0000

    Started 2d array

--- main8.c
+++ main8.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+/* Write a C program which reads a 5x5 array from a
+file and finds the maximum value in the array. The
+file stores float data in csv format. Each line
+contains 5 floats, separated by commas. There
+are 5 lines in the file. There are no commas at the
+end of lines.
+You may assume that the file does not contain
+errors.*/
+
+void max(float data);
 
 int main () {
-   
+    float data[5][5]
+    int row, col;
+}
+
+void max(float data) {
+    
 }
